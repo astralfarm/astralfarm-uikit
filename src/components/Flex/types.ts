1a2e3275fff21922a65f532cab4fb853d4156b0e
astralfarm <astralfarm0@gmail.com>
2022-01-18 12:51:56 +0800

    a

--- src/components/Flex/types.ts
+++ src/components/Flex/types.ts
@@ -1,14 +1,7 @@
 import { HTMLAttributes } from "react";
 
-import {
-  BackgroundProps,
-  BorderProps,
-  FlexboxProps,
-  LayoutProps,
-  PositionProps,
-  SpaceProps,
-  GridProps as _GridProps,
-} from "styled-system";
+import { BackgroundProps, BorderProps, FlexboxProps, LayoutProps, PositionProps, SpaceProps } from "styled-system";
+
 export interface BoxProps
   extends BackgroundProps,
     BorderProps,
